66bdc83fe22f3777bed9ace90f3036b65ea4dff1
Pranav Kaul <66698881+pranavkaul@users.noreply.github.com>
2021-03-30 18:47:27 +0530

    Update Rising_Temperature.sql

--- Solutions/Rising_Temperature.sql
+++ Solutions/Rising_Temperature.sql
@@ -1,2 +1,9 @@
 SELECT w2.Id
 FROM Weather w1 JOIN Weather w2 ON DATEDIFF(w2.recordDate,w1.recordDate) = 1 AND w1.Temperature < w2.Temperature;
+
+Oracle Solution
+
+select W1.id
+from Weather W1 , Weather W2
+where W1.Temperature > W2.Temperature
+and W2.recordDate = W1.recordDate -1;

26ce01a36a4ba32c80ff48324886973af3dcc7b2
Pranav Kaul <66698881+pranavkaul@users.noreply.github.com>
2021-04-08 16:49:13 +0530

    Create Trips_and_Users.sql

---
+++ Solutions/Trips_and_Users.sql
@@ -0,0 +1,4 @@
+SELECT T1.Request_at as Day, ROUND(SUM(T1.status != 'completed') / COUNT(*),2) AS 'Cancellation rate'
+FROM Trips T1 JOIN Users U1 ON T1.Client_Id = U1.Users_Id AND U1.Banned = 'No'
+WHERE T1.Request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY T1.Request_at;

cff7dd392f971a9e3dce2e6b5048ca7965bf2d91
Pranav Kaul <66698881+pranavkaul@users.noreply.github.com>
2021-04-15 14:00:44 +0530

    Update Game_Play_Analysis_III.sql

--- Solutions/Game_Play_Analysis_III.sql
+++ Solutions/Game_Play_Analysis_III.sql
@@ -45,3 +45,8 @@ Note that for each player we only care about the days when the player logged in.
 SELECT a1.player_id, a1.event_date, SUM(a2.games_played) AS games_played_so_far FROM Activity a1
 JOIN Activity a2 ON a1.event_date >= a2.event_date AND a2.player_id = a1.player_id  
 GROUP BY a1.player_id, a1.event_date;
+
+
+'Using Window Functions'
+
+SELECT player_id, event_date, SUM(games_played) OVER (PARTITION BY player_id ORDER BY event_date) AS games_played_so_far FROM Activity;

fa656253e4df86fc5451e4156daef64a7980d339
Pranav Kaul <66698881+pranavkaul@users.noreply.github.com>
2021-04-24 19:07:06 +0530

    Update Game_Play_Analysis_IV.sql

--- Solutions/Game_Play_Analysis_IV.sql
+++ Solutions/Game_Play_Analysis_IV.sql
@@ -7,7 +7,6 @@
 | games_played | int     |
 +--------------+---------+
 (player_id, event_date) is the primary key of this table.
-This table shows the activity of players of some game.
 Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on some day using some device.
  
 
